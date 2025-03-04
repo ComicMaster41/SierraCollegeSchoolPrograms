583102bcd4c4807c0e94253718ad68d41ac2a4cd
Andres Lauder <80169543+ComicMaster41@users.noreply.github.com>
2025-03-04 02:41:06 -0800

    Add files via upload

---
+++ C Programming/libsocket/C++/CMakeFiles/socket++_o.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/libsocket/C++/dgramclient.cpp" "/home/libsocket/C++/CMakeFiles/socket++_o.dir/dgramclient.cpp.o"
+  "/home/libsocket/C++/dgramoverstream.cpp" "/home/libsocket/C++/CMakeFiles/socket++_o.dir/dgramoverstream.cpp.o"
+  "/home/libsocket/C++/exception.cpp" "/home/libsocket/C++/CMakeFiles/socket++_o.dir/exception.cpp.o"
+  "/home/libsocket/C++/framing.cpp" "/home/libsocket/C++/CMakeFiles/socket++_o.dir/framing.cpp.o"
+  "/home/libsocket/C++/inetbase.cpp" "/home/libsocket/C++/CMakeFiles/socket++_o.dir/inetbase.cpp.o"
+  "/home/libsocket/C++/inetclientdgram.cpp" "/home/libsocket/C++/CMakeFiles/socket++_o.dir/inetclientdgram.cpp.o"
+  "/home/libsocket/C++/inetclientstream.cpp" "/home/libsocket/C++/CMakeFiles/socket++_o.dir/inetclientstream.cpp.o"
+  "/home/libsocket/C++/inetdgram.cpp" "/home/libsocket/C++/CMakeFiles/socket++_o.dir/inetdgram.cpp.o"
+  "/home/libsocket/C++/inetserverdgram.cpp" "/home/libsocket/C++/CMakeFiles/socket++_o.dir/inetserverdgram.cpp.o"
+  "/home/libsocket/C++/inetserverstream.cpp" "/home/libsocket/C++/CMakeFiles/socket++_o.dir/inetserverstream.cpp.o"
+  "/home/libsocket/C++/select.cpp" "/home/libsocket/C++/CMakeFiles/socket++_o.dir/select.cpp.o"
+  "/home/libsocket/C++/socket.cpp" "/home/libsocket/C++/CMakeFiles/socket++_o.dir/socket.cpp.o"
+  "/home/libsocket/C++/streamclient.cpp" "/home/libsocket/C++/CMakeFiles/socket++_o.dir/streamclient.cpp.o"
+  "/home/libsocket/C++/unixbase.cpp" "/home/libsocket/C++/CMakeFiles/socket++_o.dir/unixbase.cpp.o"
+  "/home/libsocket/C++/unixclientdgram.cpp" "/home/libsocket/C++/CMakeFiles/socket++_o.dir/unixclientdgram.cpp.o"
+  "/home/libsocket/C++/unixclientstream.cpp" "/home/libsocket/C++/CMakeFiles/socket++_o.dir/unixclientstream.cpp.o"
+  "/home/libsocket/C++/unixdgram.cpp" "/home/libsocket/C++/CMakeFiles/socket++_o.dir/unixdgram.cpp.o"
+  "/home/libsocket/C++/unixserverdgram.cpp" "/home/libsocket/C++/CMakeFiles/socket++_o.dir/unixserverdgram.cpp.o"
+  "/home/libsocket/C++/unixserverstream.cpp" "/home/libsocket/C++/CMakeFiles/socket++_o.dir/unixserverstream.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "MIXED"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
